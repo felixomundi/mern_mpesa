90da434fbbdc05a8714d1e9e7eb1f7e3ddc7ff15
Felix Omundi <omundifelix30@gmail.com>
2024-01-11 13:38:16 +0300

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors =require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors({
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/api/mpesa", require("./src/routes/mpesaRoute"));
+// app.use("/api/v1/payments", require("./src/routes/mpesaRoute"));
 
 app.listen(
    PORT,
     () => {
-    console.log(`Server up and running on ${PORT}...`);
+    console.log(`Server up and running on ${PORT}`);
     }
 );
